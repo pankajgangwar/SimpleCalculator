faf2dea815ef3eb979ced369a7eb4af516234c8c
Pankaj Kumar <pankaj.gat@gmail.com>
2015-06-15 11:04:53 +0530

    Initial commit, Add all project files

---
+++ app/src/main/java/utils/Utility.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * Created by iriemo on 7/5/15.
+ */
+public class Utility {
+
+    public static final String PREF_NAME = "MY_PREF";
+    public static final String KEY_ARGS = "POSITION";
+
+}
